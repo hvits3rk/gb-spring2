d2d1e286460af186d022ed0b06b0a3e0eb3d9932
Roman Tupikov <romkacg@gmail.com>
2018-08-04 00:45:50 +0300

    git init

---
+++ src/main/java/com/romantupikov/gbspring/GbspringApplication.java
@@ -0,0 +1,12 @@
+package com.romantupikov.gbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbspringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/romantupikov/gbspring/GbspringApplicationTests.java
@@ -0,0 +1,19 @@
+package com.romantupikov.gbspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GbspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
