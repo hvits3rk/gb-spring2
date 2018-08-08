dc0e0fcd3bd96950e4da0dd8460475d7f11ca65b
Roman Tupikov <romkacg@gmail.com>
2018-08-09 01:53:25 +0300

    Logging with AOP added; some SPeL tests

---
+++ src/main/java/com/romantupikov/gbspring/aop/aspect/LogAspect.java
@@ -0,0 +1,43 @@
+package com.romantupikov.gbspring.aop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("within(@org.springframework.stereotype.Service *)")
+    public void serviceClassMethods() {}
+
+    // Кидает логи затраченного времени на выполнение методов класса, помеченного аннотацией @Service
+    @Around("serviceClassMethods()")
+    public Object logServiceMethodsExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        log.info("{} executed in {}ms", joinPoint.getSignature().toShortString(), executionTime);
+
+        return proceed;
+    }
+
+    // Логгирует перед вызовом метода, помеченного кастомной аннотацией (@LogBefore)
+    @Before("@annotation(com.romantupikov.gbspring.aop.annotation.LogBefore)")
+    public void logBefore(JoinPoint joinPoint) throws Throwable {
+        log.info("{}", joinPoint);
+    }
+
+}
--- src/main/java/com/romantupikov/gbspring/security/repository/RoleRepository.java
+++ src/main/java/com/romantupikov/gbspring/security/repository/RoleRepository.java
@@ -1,11 +1,15 @@
 package com.romantupikov.gbspring.security.repository;
 
+import com.romantupikov.gbspring.aop.annotation.LogBefore;
 import com.romantupikov.gbspring.security.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
 
+    @LogBefore
     Optional<Role> findByRoleName(String name);
 }
