39ffb9ed8233ebecfc2cc538e61e3262a787d3ca
Roman Tupikov <romkacg@gmail.com>
2018-08-26 17:03:02 +0300

    DOcker image config for deploy

--- src/main/java/com/romantupikov/gbspring/aop/aspect/LogAspect.java
+++ src/main/java/com/romantupikov/gbspring/aop/aspect/LogAspect.java
@@ -1,13 +1,6 @@
 package com.romantupikov.gbspring.aop.aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
